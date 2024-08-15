ad8e905b2e9edf2d1703e5e087b48fed253455f9
Skydiver-Web <126831858+Skydiver-Web@users.noreply.github.com>
2024-08-15 02:38:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,59 @@
-import express from 'express';
-import http from 'node:http';
-import createBareServer from "@tomphttp/bare-server-node";
-import path from 'node:path';
-import cors from 'cors';
+import express from 'express'
+import basicAuth from 'express-basic-auth'
+import http from 'node:http'
+import { createBareServer } from '@tomphttp/bare-server-node'
 import { fileURLToPath } from "url";
 import { createServer as createHttpsServer } from "node:https";
 import { createServer as createHttpServer } from "node:http";
 import { readFileSync, existsSync } from "node:fs";
 import serveStatic from "serve-static";
+import path from 'node:path'
+import cors from 'cors'
+import config from './config.js'
 
-const __dirname = process.cwd();
-const server = http.createServer();
-const app = express(server);
-const bareServer = createBareServer('/bare/');
-const PORT = 8080;
+const __dirname = process.cwd()
+const server = http.createServer()
+const app = express(server)
+const bareServer = createBareServer('/bare/')
+const PORT = process.env.PORT || 8080
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(express.static(path.join(__dirname, 'static')));
+if (config.challenge) {
+  console.log('Password protection is enabled. Usernames are: ' + Object.keys(config.users))
+  console.log('Passwords are: ' + Object.values(config.users))
 
-const routes = [
-  { path: '/', file: 'index.html' },
-  { path: '/home', file: 'index.html' },
-  { path: '/games', file: 'games.html' },
-  { path: '/games/roblox', file: 'games.html' },
-  { path: '/ai', file: 'ai.html' },
-  { path: '/play/roblox-corporation/5349/roblox', file: 'games.html' },
-];
+  app.use(
+    basicAuth({
+      users: config.users,
+      challenge: true,
+    })
+  )
+}
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use(express.static(path.join(__dirname, 'static')))
+
+if (config.routes !== false) {
+  const routes = [
+    { path: '/', file: 'index.html' },
+    { path: '/home', file: 'index.html' },
+    { path: '/gyat', file: 'index.html' },
+    { path: '/games', file: 'games.html' },
+    { path: '/games/roblox', file: 'games.html' },
+    { path: '/ai', file: 'ai.html' },
+    { path: '/play/roblox-corporation/5349/roblox', file: 'games.html' },
+    { path: '/lol', file: '/lol/index.html' },
+    { path: '/referrals', file: '/referrals/index.html' },
+    { path: '/ref', file: '/referrals/index.html' },
+  ];
+
+  routes.forEach((route) => {
+    app.get(route.path, (req, res) => {
+      res.sendFile(path.join(__dirname, 'static', route.file))
+    })
+  })
+}
 
 app.get('/edu/*', cors({ origin: false }), async (req, res, next) => {
   try {
@@ -46,35 +72,61 @@ app.get('/edu/*', cors({ origin: false }), async (req, res, next) => {
   }
 });
 
-routes.forEach((route) => {
-  app.get(route.path, (req, res) => {
-    res.sendFile(path.join(__dirname, 'static', route.file));
-  });
+
+const fetchData = async (req, res, next, baseUrls) => {
+  try {
+    const reqTarget = baseUrls.map((baseUrl) => `${baseUrl}/${req.params[0]}`)
+    let data
+    let asset
+
+    for (const target of reqTarget) {
+      asset = await fetch(target)
+      if (asset.ok) {
+        data = await asset.arrayBuffer()
+        break
+      }
+    }
+
+    if (data) {
+      res.end(Buffer.from(data))
+    } else {
+      res.status(404).send()
+    }
+  } catch (error) {
+    console.error(`Error fetching ${req.url}:`, error)
+    res.status(500).send()
+  }
+}
+
+app.get('*', (req, res) => {
+  res.status(404).send();
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send();
 });
 
 server.on('request', (req, res) => {
   if (bareServer.shouldRoute(req)) {
-    bareServer.routeRequest(req, res);
+    bareServer.routeRequest(req, res)
   } else {
-    app(req, res);
+    app(req, res)
   }
-});
+})
 
-server.on("upgrade", (req, socket, head) => {
-  if(bare.shouldRoute(req, socket, head)) bare.routeUpgrade(req, socket, head); else socket.end();
-});
-
+server.on('upgrade', (req, socket, head) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeUpgrade(req, socket, head)
+  } else {
+    socket.end()
+  }
+})
 
 server.on('listening', () => {
-  const addr = server.address();
+  console.log(`Running at http://localhost:${PORT}`)
+})
 
-  console.log(`Server running on port ${addr.port}`)
-	console.log('');
-  console.log('You can now view it in your browser.')
-  /* Code for listing IPS from website-aio */
-	console.log(`Local: http://${addr.family === 'IPv6' ? `[${addr.address}]` : addr.address}:${addr.port}`);
-	try { console.log(`On Your Network: http://${address.ip()}:${addr.port}`); } catch (err) {/* Can't find LAN interface */};
-	if(process.env.REPL_SLUG && process.env.REPL_OWNER) console.log(`Replit: https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`);
-});
-
-server.listen({ port: (process.env.PORT || 8080) })
+server.listen({
+  port: PORT,
+})
